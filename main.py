f4a11891744281c7b1a11a94377825651595ebf1
joaobjsantos <93948049+joaobjsantos@users.noreply.github.com>
2021-11-15 12:38:08 +0000

    Código para os gráficos de 4/5 ficheiros
    O ficheiro .DS_Store não deu para por no pycharm por alguma razão.

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello World!")
+import matplotlib.pyplot as plt
+
+
+file_names = ["bible.txt", "finance.csv", "jquery-3.6.0.js","random.txt"]
+for i in file_names:
+    file = ""
+    with open("dataset\\"+i, "r") as f:
+        file = f.read()
+    ocorr = {}
+    for j in file:
+        if j not in ocorr:
+            ocorr[j] = 1
+        else:
+            ocorr[j] += 1
+    ocorr = list(ocorr.items())
+    ocorr = dict(sorted(ocorr, key=lambda x: x[1], reverse=True))
+    plt.plot(list(ocorr.keys()),list(ocorr.values()))
+    plt.title(i)
+    plt.show()
