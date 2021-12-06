314ce41d16260c42466cd9331bf5c052524582f4
Gonçalo Almeida <gamertota4@gmail.com>
2021-12-06 10:49:27 +0000

    lzw tem um erro no decompress

--- Semana4e5/Main.py
+++ Semana4e5/Main.py
@@ -2,6 +2,7 @@ import bz2
 import lzma
 
 from HuffmanCodec import HuffmanCodec as huffman
+import LZW as lzw
 
 
 def huffman_coding(data):
@@ -13,29 +14,42 @@ def huffman_coding(data):
 
 def bzip(data):
     compressed_data = bz2.compress(data.encode())
-    with open("..\compressed_dataset\\bible.bzip2", "wb") as write_file:
-        write_file.write(compressed_data)
     decompressed_data = bz2.decompress(compressed_data)
     str_decompressed_data = decompressed_data.decode()
     # print(str_decompressed_data)
     print(str_decompressed_data == data)  # True
+    return compressed_data, decompressed_data
 
 
 def lzma_encoding(data):
     compressed_data = lzma.compress(str.encode(data))
-    with open("..\compressed_dataset\\bible.lzma", "wb") as write_file:
-        write_file.write(compressed_data)
     decompressed_data = lzma.decompress(compressed_data)
     str_decompressed_data = decompressed_data.decode()
     # print(str_decompressed_data)
     print(str_decompressed_data == data)  # True
+    return compressed_data, decompressed_data
 
 
-filenames = ["bible.txt", "finance.csv", "jquery-3.6.0.js", "random.txt"]
-for i in filenames:
-    with open("..\dataset\\" + i, "r") as file:
+def lzw_encoding(data):
+    compressed_data = lzw.compress(data.encode)
+    decompressed_data = lzw.decompress(compressed_data)
+    str_decompressed_data = decompressed_data.join()
+    print(str_decompressed_data == data)  # True
+    return compressed_data, decompressed_data
+
+
+data_type = [".txt", ".csv", ".js", ".txt"]
+filenames = ["bible", "finance", "jquery-3.6.0", "random"]
+for i in range(len(filenames)):
+    with open("..\dataset\\" + filenames[i] + data_type[i], "r") as file:
         data = file.read()
-        bzip(data)
+        compressed_data, decompressed_data = bzip(data)
+        with open("..\dataset\\" + filenames[i] + ".bzip", "wb") as write_file:
+            write_file.write(compressed_data)
         lzma_encoding(data)
-
+        with open("..\dataset\\" + filenames[i] + ".lzma", "wb") as write_file:
+            write_file.write(compressed_data)
+        lzw_encoding(data)
+        with open("..\dataset\\" + filenames[i] + ".lzw", "wb") as write_file:
+            write_file.write(compressed_data)
         huffman_coding(data)
