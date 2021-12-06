1bab5cafcdb140ff5fb1d51998e5e591911f88b0
Gonçalo Almeida <gamertota4@gmail.com>
2021-12-06 10:25:08 +0000

    Main

--- Semana4e5/Main.py
+++ Semana4e5/Main.py
@@ -1,13 +1,41 @@
+import bz2
+import lzma
+
 from HuffmanCodec import HuffmanCodec as huffman
-#tam da tabela - simbolos - comprimento - numero de bytes
-with open("..\dataset\\bible.txt", "r") as file:
-    linha = file.read()
-    codec = huffman.from_data(linha)
+
+
+def huffman_coding(data):
+    codec = huffman.from_data(data)
     # Code table is dictionary mapping symbol to (bitsize, value)
     code_table = huffman.get_code_table(codec)
-    print(code_table)
-    #print(linha)
-    #compresso = codec.encode(linha)
-    #print(compresso)
-    #print(codec.decode(compresso))
-    #print(code_table)
+    # print(code_table)
+
+
+def bzip(data):
+    compressed_data = bz2.compress(data.encode())
+    with open("..\compressed_dataset\\bible.bzip2", "wb") as write_file:
+        write_file.write(compressed_data)
+    decompressed_data = bz2.decompress(compressed_data)
+    str_decompressed_data = decompressed_data.decode()
+    # print(str_decompressed_data)
+    print(str_decompressed_data == data)  # True
+
+
+def lzma_encoding(data):
+    compressed_data = lzma.compress(str.encode(data))
+    with open("..\compressed_dataset\\bible.lzma", "wb") as write_file:
+        write_file.write(compressed_data)
+    decompressed_data = lzma.decompress(compressed_data)
+    str_decompressed_data = decompressed_data.decode()
+    # print(str_decompressed_data)
+    print(str_decompressed_data == data)  # True
+
+
+filenames = ["bible.txt", "finance.csv", "jquery-3.6.0.js", "random.txt"]
+for i in filenames:
+    with open("..\dataset\\" + i, "r") as file:
+        data = file.read()
+        bzip(data)
+        lzma_encoding(data)
+
+        huffman_coding(data)
