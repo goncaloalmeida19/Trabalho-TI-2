b2f04b55a14cef083cba274466c32d0bb9ea8f76
Gonçalo Almeida <gamertota4@gmail.com>
2021-12-13 11:39:08 +0000

    Interface

--- Semana4e5/Main.py
+++ Semana4e5/Main.py
@@ -1,28 +1,36 @@
-# ppm https://github.com/nayuki/Reference-arithmetic-coding/tree/master/python
 import bz2
 import lzma
 import os
+import time
 
 import pyppmd
-import HuffmanCodec
 
+import HuffmanCodec
 import LZW as lzw
 from Semana4e5 import Deflate
 
 
 def huffman_encoding(data):
+    start = time.perf_counter()
     encoded_text = HuffmanCodec.encode(data)
     compressed_data = bytearray()
     for i in range(0, len(encoded_text), 8):
         compressed_data.append(int(encoded_text[i:i + 8], 2))
+    print("Compression Huffman: ", round(time.perf_counter() - start, 5), "seg")
+    start = time.perf_counter()
     decompressed_data = HuffmanCodec.decode(encoded_text)
+    print("Deompression Huffman: ", round(time.perf_counter() - start, 5), "seg")
     print("Huffman:", decompressed_data == data)  # True
     return compressed_data, decompressed_data
 
 
 def bzip(data):
+    start = time.perf_counter()
     compressed_data = bz2.compress(data.encode())
+    print("Compression Bzip2: ", round(time.perf_counter() - start, 5), "seg")
+    start = time.perf_counter()
     decompressed_data = bz2.decompress(compressed_data)
+    print("Decompression Bzip2: ", round(time.perf_counter() - start, 5), "seg")
     str_decompressed_data = decompressed_data.decode()
     # print(str_decompressed_data)
     print("Bzip2:", str_decompressed_data == data)  # True
@@ -30,8 +38,12 @@ def bzip(data):
 
 
 def lzma_encoding(data):
+    start = time.perf_counter()
     compressed_data = lzma.compress(str.encode(data))
+    print("Compression LZMA: ", round(time.perf_counter() - start, 5), "seg")
+    start = time.perf_counter()
     decompressed_data = lzma.decompress(compressed_data)
+    print("Decompression LZMA: ", round(time.perf_counter() - start, 5), "seg")
     str_decompressed_data = decompressed_data.decode()
     # print(str_decompressed_data)
     print("LZMA:", str_decompressed_data == data)  # True
@@ -47,15 +59,23 @@ def lzw_encoding(data):
 
 
 def ppm_encoding(data):
+    start = time.perf_counter()
     compressed_data = pyppmd.compress(data.encode())
+    print("Compression PPM: ", round(time.perf_counter() - start, 5), "seg")
+    start = time.perf_counter()
     decompressed_data = pyppmd.decompress(compressed_data).decode()
+    print("Decompression PPM: ", round(time.perf_counter() - start, 5), "seg")
     print("PPM:", decompressed_data == data)  # True
     return compressed_data, decompressed_data
 
 
 def deflate_encoding(data):
+    start = time.perf_counter()
     compressed_data = Deflate.deflate(data.encode())
+    print("Compression Deflate: ", round(time.perf_counter() - start, 5), "seg")
+    start = time.perf_counter()
     decompressed_data = Deflate.inflate(compressed_data)
+    print("Decompression Deflate: ", round(time.perf_counter() - start, 5), "seg")
     str_decompressed_data = decompressed_data.decode()
     print("Deflate:", str_decompressed_data == data)  # True
     return compressed_data, decompressed_data
@@ -77,33 +97,27 @@ for i in range(len(filenames)):
     read_path = "..\dataset\\" + filenames[i] + data_type[i]
     with open(read_path, "r") as file:
         data = file.read()
-        write_path = "..\compressed_dataset\\" + filenames[i]
-        compressed_data_bzip, decompressed_data_bzip = bzip(data)
-        with open(write_path + compression_type[j], "wb") as write_file:
-            write_file.write(compressed_data_bzip)
-        get_ratio(read_path, write_path + compression_type[j])
-        compressed_data_lzma, decompressed_data_lzma = lzma_encoding(data)
-        j += 1
-        with open(write_path + compression_type[j], "wb") as write_file:
-             write_file.write(compressed_data_lzma)
-        get_ratio(read_path, write_path + compression_type[j])
-        # compressed_data_lzw, decompressed_data_lzw = lzw_encoding(data)
-        #compressed_data_lzw = lzw_encoding(data)
-        j += 1
-        #with open(write_path + compression_type[j], "wb") as write_file:
-        #    write_file.write(compressed_data_lzw.encode())
-        compressed_data_huffman, decompressed_data_huffman = huffman_encoding(data)
-        j += 1
-        with open(write_path + compression_type[j], "wb") as write_file:
-            write_file.write(compressed_data_huffman)
-        get_ratio(read_path, write_path + compression_type[j])
-        compressed_data_ppm, decompressed_data_ppm = ppm_encoding(data)
-        j += 1
-        with open(write_path + compression_type[j], "wb") as write_file:
-            write_file.write(compressed_data_ppm)
-        get_ratio(read_path, write_path + compression_type[j])
-        compressed_data_deflate, decompressed_data_deflate = deflate_encoding(data)
-        j += 1
-        with open(write_path + compression_type[j], "wb") as write_file:
-            write_file.write(compressed_data_deflate)
-        get_ratio(read_path, write_path + compression_type[j])
+        for j in range(len(compression_type)):
+            write_path = "..\compressed_dataset\\" + filenames[i] + compression_type[j]
+            with open(write_path, "wb") as write_file:
+                if j == 0:
+                    compressed_data_bzip, decompressed_data_bzip = bzip(data)
+                    write_file.write(compressed_data_bzip)
+                elif j == 1:
+                    compressed_data_lzma, decompressed_data_lzma = lzma_encoding(data)
+                    write_file.write(compressed_data_lzma)
+                elif j == 2:
+                    pass
+                    # compressed_data_lzw, decompressed_data_lzw = lzw_encoding(data)
+                    # with open(write_path, "wb") as write_file:
+                    #    write_file.write(compressed_data_lzw.encode())
+                elif j == 3:
+                    compressed_data_huffman, decompressed_data_huffman = huffman_encoding(data)
+                    write_file.write(compressed_data_huffman)
+                elif j == 4:
+                    compressed_data_ppm, decompressed_data_ppm = ppm_encoding(data)
+                    write_file.write(compressed_data_ppm)
+                elif j == 5:
+                    compressed_data_deflate, decompressed_data_deflate = deflate_encoding(data)
+                    write_file.write(compressed_data_deflate)
+            get_ratio(read_path, write_path)
