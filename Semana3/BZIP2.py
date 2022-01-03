70cf87af79b229de3f96b42a6a4d79fdc94c872c
Gonçalo Almeida <52497560+goncaloalmeida19@users.noreply.github.com>
2022-01-03 18:03:50 +0000

    Delete Semana3 directory

--- Semana3/BZIP2.py
+++
@@ -1,7 +0,0 @@
-#import bz2
-#
-#fich = ""
-#
-#with open(fich, 'rb') as data:
-#    comprimido = bz2.compress(data.read(), 9)
-#    print(comprimido)
--- Semana3/LZMA.py
+++
@@ -1,6 +0,0 @@
-import lzma
-
-
-def compressor(s):
-    t = lzma.LZMACompressor().compress(s)
-    return t
